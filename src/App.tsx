6dbf6f84878e5965704600748ef8fb0878848bd6
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-07-20 00:05:20 +0630

    Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Button from '@material-ui/core/Button';
+import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Ayar Hlaine
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={'Bac_Wrapper'}>
+        <div className={'B_Wrapper'}>
+          <h1 className="animate__animated animate__backInDown">Font End Developer</h1>
+          <Button variant="contained" color="primary" className={'animate__animated animate__heartBeat Hire__Button'}>
+            Hire
+          </Button>
+        </div>
+      </div>
+      <div className={'TextSection'}>
+            <h4> I'm Ayar Hlaine</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, illum repellendus inventore, temporibus delectus, maxime molestiae excepturi nostrum non doloribus beatae amet sequi aspernatur! Fugit, atque. Minus quo obcaecati asperiores.</p>
+      </div>
     </div>
   );
 }
