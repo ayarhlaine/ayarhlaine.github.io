2349397823bcfc62dffb47debe1140597e4f3f2e
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-04 22:10:47 +0630

    Added Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
+import Banner from './Banner/Banner';
 import './App.scss';
 function App() {
   return (
-    <div className="App">
-      <div className={'Bac_Wrapper'}>
-        <div className={'B_Wrapper'}>
-          <h1 className="animate__animated animate__backInDown">Font End Developer</h1>
-          <Button variant="contained" color="primary" className={'animate__animated animate__heartBeat Hire__Button'}>
-            Hire
-          </Button>
-        </div>
-      </div>
-      <div className={'TextSection'}>
-            <h4> I'm Ayar Hlaine</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, illum repellendus inventore, temporibus delectus, maxime molestiae excepturi nostrum non doloribus beatae amet sequi aspernatur! Fugit, atque. Minus quo obcaecati asperiores.</p>
-      </div>
+    <div className="app">
+      <Banner />
+      {/* About Me */}
+      {/* Education */}
     </div>
   );
 }
---
+++ src/Banner/Banner.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Banner.scss';
+function Banner() {
+    return (
+        <div className="banner">
+            <div className="bannerLeft">
+                <h2 className="bannerLeft__header">I am 
+                    <span className="bannerLeft__header__name">&nbsp;Ayar Hlaine</span>
+                </h2>
+                <p className="bannerLeft__text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit nostrum nihil nulla molestiae amet ex assumenda nesciunt id, similique neque sequi fugiat a! Mollitia accusamus exercitationem maxime eius! Aspernatur, vel!</p>
+            </div>
+            <div className="bannerRight">
+
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
